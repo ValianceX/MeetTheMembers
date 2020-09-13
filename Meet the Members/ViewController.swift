474981353c5cf082d86cd2e401800c5d0812bae1
sydney <sydney.karimi02@gmail.com>
2020-09-12 20:05:26 -0700

    Initial Commit

---
+++ Meet the Members/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meet the Members
+//
+//  Created by Sydney Karimi on 9/12/20.
+//  Copyright © 2020 Sydney Karimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
